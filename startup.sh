988ffad7100f3861d35e49ef94634f8fa3a660b9
daniilprohorov <daniilprohorov@gmail.com>
2024-10-09 03:48:45 +0300

    wip

--- startup.sh
+++ startup.sh
@@ -1,2 +1,1 @@
-node index.js &
-chromium-browser --noerrdialogs --disable-infobars --start-fullscreen 'http://localhost:3000/'
+node index.js && chromium-browser --noerrdialogs --disable-infobars --start-fullscreen 'http://localhost:3000/'
